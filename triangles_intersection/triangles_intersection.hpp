be8e03478cb75e2b7786a35e2b6f085922d35054
archie230 <43552079+archie230@users.noreply.github.com>
2019-10-13 20:13:21 +0300

    Add files via upload

--- triangles_intersection/triangles_intersection.hpp
+++ triangles_intersection/triangles_intersection.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <vector>
 #include <cstdint>
 #include <cmath>
@@ -6,7 +7,7 @@
 #include <iostream>
 
 namespace triangles {
-    extern const float TOLERANCE;
+    const float TOLERANCE = 1e-4;
     bool nearly_equal(float a, float b);
 // for ease of coding we will treat a 2D point and a 2D vector
 // as the same
@@ -14,9 +15,10 @@ namespace triangles {
         float x;
         float y;
 
-        Point2D(float x = 0, float y = 0);
-
-        ~Point2D() = default;
+        Point2D(float x = 0, float y = 0)
+                :
+                x(x),
+                y(y) {}
     };
 
     enum order {
@@ -25,31 +27,32 @@ namespace triangles {
         counterclock_o = 2
     };
 
-// Slope of line segment (p1, p2): σ = (y2 - y1)/(x2 - x1)
-// Slope of line segment (p2, p3): τ = (y3 - y2)/(x3 - x2)
-//
-// If  σ > τ, the orientation is clockwise (right turn)
-//
-// Using above values of σ and τ, we can conclude that,
-// the orientation depends on sign of  below expression:
-//
-// (y2 - y1)*(x3 - x2) - (y3 - y2)*(x2 - x1)
-
-// Above expression is negative when σ < τ, i.e.,  counterclockwise
     int orientation(const Point2D& p1, const Point2D& p2, const Point2D& p3);
 
 // overloaded operators
-    bool operator== (const Point2D& left, const Point2D& right);
-    bool operator!= (const Point2D& left, const Point2D& right);
-// math operations with Point2Ds(vectors)
-// vector2D - vector2D
-    Point2D operator- (const Point2D& left, const Point2D& right);
-// scalar * vector2D
-    Point2D operator* (float scalar, const Point2D& vec);
-// vector2D + vector2D
-    Point2D operator+ (const Point2D& left, const Point2D& right);
-// sqal(vector2D,vector2D)
-    float operator* (const Point2D& left, const Point2D& right);
+    inline bool operator==(const Point2D &left, const Point2D &right) {
+        return nearly_equal(left.x, right.x) && nearly_equal(left.y, right.y);
+    }
+
+    inline bool operator!=(const Point2D &left, const Point2D &right) {
+        return !(left == right);
+    }
+
+    inline Point2D operator-(const Point2D &left, const Point2D &right) {
+        return {left.x - right.x, left.y - right.y};
+    }
+// multiplying scalar ang vector
+    inline Point2D operator*(float scalar, const Point2D &vec) {
+        return {scalar * vec.x, scalar * vec.y};
+    }
+
+    inline Point2D operator+(const Point2D &left, const Point2D &right) {
+        return {left.x + right.x, left.y + right.y};
+    }
+// scalar vector multiplying
+    inline float operator*(const Point2D &left, const Point2D &right) {
+        return left.x * right.x + left.y * right.y;
+    }
 
     struct Triangle {
         Point2D pts_[3];
@@ -59,29 +62,34 @@ namespace triangles {
 
         Triangle();
 
-        ~Triangle() = default;
-
         // set counter clockwise order
         int SetCounterClock();
 
         // calculating internal normals
         void CalculateNormals();
 
-        bool IsTrivial() const;
+        bool IsTrivial() const {
+            return orientation(pts_[0], pts_[1], pts_[2]) == 0;
+        }
     };
 
-    std::istream&  operator >> (std::istream& is, Triangle& tr);
+    std::istream&  operator>> (std::istream& is, Triangle& tr);
 
     struct Polygon {
         std::vector<Point2D> pts_;
 
-        explicit Polygon(const std::vector<Point2D>& pts);
+        explicit Polygon(const std::vector<Point2D>& pts)
+                :
+                pts_(pts)
+        {}
 
-        explicit Polygon(uint32_t num);
+        explicit Polygon(uint32_t num)
+                :
+                pts_(std::vector<Point2D>(num))
+        {}
 
         Polygon() = default;
 
-        ~Polygon() = default;
 
         float area();
     };
@@ -130,8 +138,8 @@ namespace triangles {
  * @param p1 first segment point
  * @param p2 second segment point
  * @param tr triangle window should have counter clock order
- * @param start first clipped segment point
- * @param finish second segment point
+ * @param rp first clipped segment point
+ * @param q second segment point
  * @return bool var which true if segment is visible at window else false
  */
     bool CBClip (const Point2D& p1, const Point2D& p2, const Triangle& tr, Point2D& start, Point2D& finish);
@@ -146,7 +154,7 @@ namespace triangles {
 /**
  * @param T1 triangle should have counter clock order
  * @param T2 triangle should have counter clock order
- * @param poly reference to polygon which will contain intersection polygon
+ * @param poly reference to polygon which would contain intersection polygon
  * @return number of intersection polygon points
  */
     int CalcIntersectionPolygon(const Triangle& T1, const Triangle& T2, Polygon& suspect1);
