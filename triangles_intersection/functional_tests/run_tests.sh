92b3bb64f864e78e5e823d9e471c251388c01491
archie230 <43552079+archie230@users.noreply.github.com>
2019-10-21 11:31:28 +0300

    tests

--- triangles_intersection/functional_tests/run_tests.sh
+++ triangles_intersection/functional_tests/run_tests.sh
@@ -6,11 +6,11 @@ echo > ../test_log
 for file in *;
 do
   ANSW=$(./../../interarea < $file)
-  EXPECT=$(cat ../results/"$file.res")
+  EXPECT=$(cat ../results/${file%.dat}.res)
   if [[ $ANSW != $EXPECT ]]; then
     STATUS="FAILED"
   else
     STATUS="OK"
   fi
-  echo "TEST $file $STATUS : answ = $ANSW  expected = $EXPECT" >> ../test_log
+  echo "TEST ${file%.dat} $STATUS : answ = $ANSW  expected = $EXPECT" >> ../test_log
 done
