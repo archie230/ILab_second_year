d62139170a8e015fdf138801652dbe349fa2880c
archie230 <43552079+archie230@users.noreply.github.com>
2019-10-21 11:35:12 +0300

    Add files via upload

--- triangles_intersection/IntersectionArea.cpp
+++ triangles_intersection/IntersectionArea.cpp
@@ -1,19 +1,24 @@
 #include "triangles_intersection.hpp"
 
-using namespace triangles;
+using triangles::Triangle;
+using triangles::operator>>;
+using triangles::Polygon;
+using triangles::CalcIntersectionPolygon;
+
+const int invalid_input = -230;
 
 int main() {
     Triangle<float> t1, t2;
 
     std::cin >> t1;
     if(!std::cin) {
-        std::cerr << "bad input";
+        std::cout << invalid_input;
         exit(EXIT_FAILURE);
     }
 
     std::cin >> t2;
     if(!std::cin) {
-        std::cerr << "bad input";
+        std::cerr << invalid_input;
         exit(EXIT_FAILURE);
     }
 
